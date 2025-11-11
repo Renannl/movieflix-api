00ed22f24c0945faa963ed81d79d3fa110aada05
Renan <mxgames02@gmail.com>
2025-11-11 01:55:40 -0300

    added verification error

--- src/server.ts
+++ src/server.ts
@@ -8,14 +8,37 @@ const prisma = new PrismaClient();
 app.get("/movies", async (_, res) => {
   const movies = await prisma.movie.findMany({
     orderBy: {
-      title: "asc"
+      title: "asc",
     },
-    include:{
+    include: {
       genres: true,
-      languages: true
-    }
+      languages: true,
+    },
   });
   res.json(movies);
+});
+
+app.use(express.json());
+
+app.post("/movies", async (req, res) => {
+
+  const {title, genre_id, language_id, oscar_count, release_date} = req.body
+
+  try{
+    await prisma.movie.create({
+      data: {
+        title: title,
+        genre_id: genre_id,
+        language_id: language_id,
+        oscar_count: oscar_count,
+        release_date: new Date(release_date),
+      },
+    });
+  }catch(error){
+    return res.status(500).send({message: "Falha ao cadastrar um filme"})
+  }
+
+  res.status(201).send();
 });
 
 app.listen(port, () => {
