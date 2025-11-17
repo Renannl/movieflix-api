896b32ea3113490a882f660892bc707553a75ed7
Renan <mxgames02@gmail.com>
2025-11-17 20:12:01 -0300

    filter by genre added

--- src/server.ts
+++ src/server.ts
@@ -103,6 +103,30 @@ app.delete("/movies/:id", async (req, res) => {
   res.status(200).send();
 });
 
+app.get("/movies/:genreName", async (req, res) => {
+
+  try{
+  const moviesFilteredByGenderName = await prisma.movie.findMany({
+    include: {
+      genres: true,
+      languages: true
+    },
+      where: {
+        genres:{
+          name:{
+            equals: req.params.genreName,
+            mode: "insensitive"
+        }
+      }
+    }
+  })
+
+  res.status(200).send(moviesFilteredByGenderName)
+  }catch(error){
+    return res.status(500).send({ message: "Falha ao atualizar um filme" });
+  }
+})
+
 app.listen(port, () => {
   console.log(`Servidor em execução na porta ${port}`);
 });
